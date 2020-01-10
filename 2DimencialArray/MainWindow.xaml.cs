07e930235c6f7d858c0e6139e8fc20a365e2cc9a
Olena Kostiuk <helena.kostyuk@gmail.com>
2020-01-10 13:02:55 +0200

    Добавьте файлы проекта.

---
+++ 2DimencialArray/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2DimencialArray
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static int n;
+        public int[,] a;
+        public int[] v;
+        public MainWindow()
+        {
+            Random random = new Random();
+            int n = 5;// random.Next(5, 10);
+            a = new int[n, n];
+            v = new int[n];
+
+            InitializeComponent();
+
+            UIGrid.Columns = n;
+            UIGrid.Rows = n;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = i + j;//random.Next(1, 10);
+                    UIGrid.Children.Add(new Button() { Content = a[i, j].ToString()});
+                }
+            }
+
+            UIGrid2.Rows = n;
+
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = i; random.Next(1, 10);
+                UIGrid2.Children.Add(new Button() { Content = v[i].ToString() }); ;
+            }
+
+            UIGrid3.Rows = n;
+
+        }
+
+        private int[] MultiplyToVector(int[,] a, int[] b)
+        {
+            if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
+            int[] r = new int[b.Length];
+            for (int i = 0; i < b.Length; i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    r[i] += a[i, j] * b[j];
+                }
+            }
+            return r;
+        }
+
+        private void btnNextTask_Click(object sender, RoutedEventArgs e)
+        {
+            Window secondWindow = new SecondWindow();
+            secondWindow.Show();
+            
+            this.Close();
+        }
+
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {   
+            int[] res = MultiplyToVector(a, v);
+
+            UIGrid3.Children.Clear();
+            for (int i = 0; i < res.Length; i++)
+            {
+                UIGrid3.Children.Add(new Button() { Content = res[i].ToString() });
+            }
+        }
+    }
+}
---
+++ 2DimencialArray/SecondWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _2DimencialArray
+{
+    /// <summary>
+    /// Логика взаимодействия для SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public static int n;
+        public int[,] c;
+        public int[,] d;
+        public int[,] res;
+        public SecondWindow()
+        {
+            InitializeComponent();
+
+            Random random = new Random();
+            int n = 5;// random.Next(5, 10);
+            c = new int[n, n];
+            d = new int[n, n];
+
+
+            UIGrid.Columns = n;
+            UIGrid.Rows = n;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    c[i, j] = i + j; //random.Next(1, 10);
+                    UIGrid.Children.Add(new Button() { Content = c[i, j].ToString() });
+                }
+            }
+
+            UIGrid2.Columns = n;
+            UIGrid2.Rows = n;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    d[i, j] = i + j;// random.Next(1, 10);
+                    UIGrid2.Children.Add(new Button() { Content = d[i, j].ToString() });
+                }
+            }
+            UIGrid3.Rows = n;
+        } 
+
+        private void btnNextTask_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (IsMultPossible(c, d) == true)
+            {
+                res = MultiplyToMatrix(c, d);
+            }
+            else
+            {
+                res[0, 0] = 0;//"Multiplication impossible";
+            }
+
+            //UIGrid3.Children.Clear();
+            for (int i = 0; i < res.GetLength(0); i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    UIGrid3.Children.Add(new Button() { Content = res[i, j].ToString() });
+                }
+            }
+        }
+
+        static int[,] MultiplyToMatrix(int[,] a, int[,] b)
+        {
+
+            int[,] r = new int[a.GetLength(0), b.GetLength(1)];
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < b.GetLength(1); j++)
+                {
+                    r[i, j] = 0;
+                    for (int k = 0; k < b.GetLength(0); k++)
+                    {
+                        r[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            return r;
+        }
+        static bool IsMultPossible(int[,] a, int[,] b)
+        {
+            bool ismultpossible = false;
+            if (a.GetLength(1) == b.GetLength(0))
+            {
+                ismultpossible = true;
+            }
+            return ismultpossible;
+        }
+
+    }
+}
